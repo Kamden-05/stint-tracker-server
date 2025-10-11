f02feaccf737cb0813768d2e3d6b4e3c70e7afcc
Kamden <kamden@agada-medical.com>
2025-10-11 18:18:09 -0500

    cleaned up files

---
+++ app/routes/stints.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, FastAPI
+from stint_core.stint_base import Stint
+
+router = APIRouter(prefix='/stints')
+
+@router.post("/{session_id}")
+def create_stint(session_id: int, stint: Stint):
+    
+    return stint
+
+@router.put("/{session_id}/update/{stint_id}")
+def update_stint(session_id: int, stint: Stint):
+    return stint
+
+@router.put("/{session_id}/end/{stint_id}")
+def end_stint(session_id: int, stint: Stint):
+    return stint
