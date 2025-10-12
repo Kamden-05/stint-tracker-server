0a9ae2ac6859d57d2f3e181ab46d2f8c3637035e
Kamden <kamden@agada-medical.com>
2025-10-11 19:24:56 -0500

    created stint and session base models

---
+++ app/models/session.py
@@ -0,0 +1,19 @@
+from sqlalchemy import String, DateTime, TIMESTAMP
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List, Optional
+from datetime import datetime
+from app.database.base_class import Base
+
+class Session(Base):
+    __tablename__ = "session"
+
+    session_id: Mapped[int] = mapped_column(primary_key=True)
+    session_type: Mapped[int]
+    session_date: Mapped[datetime] = mapped_column(DateTime)
+    sim_time: Mapped[datetime] = mapped_column(TIMESTAMP)
+    stints: Mapped[List["Stint"]] = relationship()
+    track: Mapped[str] = mapped_column(String)
+    car_class: Mapped[str] = mapped_column(String)
+    car = Mapped[str] = mapped_column(String)
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True))
+    updated_at: Mapped[Optional[datetime]] = mapped_column(TIMESTAMP(timezone=True))
---
+++ app/models/stint.py
@@ -0,0 +1,30 @@
+from sqlalchemy import ForeignKey, String, TIMESTAMP
+from sqlalchemy.orm import Mapped, mapped_column
+from typing import Optional
+from datetime import datetime
+from app.database.base_class import Base
+
+class Stint(Base):
+    __tablename__ = "stint"
+
+    stint_id: Mapped[int] = mapped_column(primary_key=True)
+    session_id: Mapped[int] = mapped_column(ForeignKey("session.id") )
+    driver_name: Mapped[str] = mapped_column(String)
+    start_time: Mapped[float]
+    length: Mapped[Optional[float]]
+    laps_completed: Mapped[int]
+    avg_lap: Mapped[Optional[float]]
+    fastest_lap: Mapped[Optional[float]]
+    out_lap: Mapped[Optional[float]]
+    in_lap: Mapped[Optional[float]]
+    start_position: Mapped[int]
+    end_position: Mapped[Optional[int]]
+    start_fuel: Mapped[float]
+    end_fuel: Mapped[Optional[float]]
+    refuel_amount: Mapped[Optional[float]]
+    tire_change: Mapped[Optional[bool]]
+    repairs: Mapped[Optional[bool]]
+    pit_service_duration: Mapped[Optional[float]]
+    incidents: Mapped[Optional[int]]
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True))
+    updated_at: Mapped[Optional[datetime]] = mapped_column(TIMESTAMP(timezone=True))
