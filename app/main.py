2fb31a206dcff4dc9bdf0e162beafef9e1ccfd6c
Kamden <kamden@agada-medical.com>
2025-10-12 15:18:49 -0500

    added routers

--- app/main.py
+++ app/main.py
@@ -3,8 +3,11 @@ from .routes import sessions, stints, laps
 
 app = FastAPI()
 
+app.include_router(sessions.router)
 app.include_router(stints.router)
+app.include_router(laps.router)
 
-@app.get('/')
+
+@app.get("/")
 def root():
     return {"message": "Hello world!"}
