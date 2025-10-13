f7e13bc77628b15462e61254e10312121c496c36
Kamden <kamden@agada-medical.com>
2025-10-13 16:50:02 -0500

    created lap post method

--- app/routes/laps.py
+++ app/routes/laps.py
@@ -1,4 +1,24 @@
-from fastapi import APIRouter, FastAPI
+from fastapi import APIRouter, Depends, HTTPException, status
 from stint_core.lap_base import LapCreate
+from app.database.db import get_db
+from typing import Annotated
+from sqlalchemy.orm import Session
+from app.repositories import stint_crud, lap_crud
+from app.models.stint import Stint
 
-router = APIRouter(prefix="/laps", tags=["laps"])
+
+router = APIRouter(prefix='/stints/{stint_id}/laps', tags=['laps'])
+
+DbSession = Annotated[Session, Depends(get_db)]
+
+@router.post("/")
+def create_lap(stint_id: int, lap_create: LapCreate, db: DbSession):
+    stint = stint_crud.get_one(db, Stint.id == stint_id)
+
+    if stint is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'Stint with id {stint_id} not found'
+        )
+    lap = lap_crud.create(db, lap_create)
+    return lap
