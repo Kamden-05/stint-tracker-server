ff154881b5f4c7cf17520e713080095e46eed29c
Kamden <kamden@agada-medical.com>
2025-10-15 23:05:58 -0500

    added sheet updating logic for Laps endpoints

--- app/routes/laps.py
+++ app/routes/laps.py
@@ -1,18 +1,18 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, BackgroundTasks
 from stint_core.lap_base import LapCreate
 from app.database.db import get_db
 from typing import Annotated
 from sqlalchemy.orm import Session
 from app.repositories import stint_crud, lap_crud
 from app.models.stint import Stint
-
+from app.services.sheets_service import update_range
 
 router = APIRouter(prefix='/stints/{stint_id}/laps', tags=['laps'])
 
 DbSession = Annotated[Session, Depends(get_db)]
 
 @router.post("/")
-def create_lap(stint_id: int, lap_create: LapCreate, db: DbSession):
+def create_lap(stint_id: int, lap_create: LapCreate, db: DbSession, background_tasks: BackgroundTasks, sheet_id: str = None, sheet_range: str = None):
     stint = stint_crud.get_one(db, Stint.id == stint_id)
 
     if stint is None:
@@ -27,5 +27,8 @@ def create_lap(stint_id: int, lap_create: LapCreate, db: DbSession):
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
             detail=f'Lap with lap number {lap_create.lap_number} already exists'
-        )
+        ) from e
+    
+    if sheet_id is not None:
+        background_tasks.add_task(update_range, sheet_id, sheet_range, stint.laps)
     return lap
