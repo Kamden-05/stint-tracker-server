057cc923fec5b0c19e4cd107c73b63dcc1f6c05d
Kamden <kamden@agada-medical.com>
2025-10-18 13:42:57 -0500

    updated stint schemas

--- app/schemas/session_schemas.py
+++ app/schemas/session_schemas.py
@@ -2,9 +2,6 @@ from pydantic import BaseModel, Field, ConfigDict
 import datetime
 
 class RaceSessionBase(BaseModel):
-    model_config = ConfigDict(frozen=True)
-
-    session_id: int = Field(..., alias="id")
     session_date: datetime.date
     track: str
     car_class: str
@@ -12,5 +9,9 @@ class RaceSessionBase(BaseModel):
     sim_date: datetime.date
     sim_time: datetime.time
 
-class RaceSessionCreate(RaceSessionBase):
+class RaceSessionRead(RaceSessionBase):
     pass
+
+class RaceSessionCreate(RaceSessionBase):
+    id: int
+
--- app/schemas/stint_schemas.py
+++ app/schemas/stint_schemas.py
@@ -3,27 +3,38 @@ from typing import List, Optional
 from app.schemas.lap_schemas import LapBase as Lap
 
 class StintBase(BaseModel):
-    session_id: int
-    stint_number: int = Field(..., alias="number")
+    number: int
     driver_name: str
     start_time: float
     start_position: int
     start_fuel: float
     end_position: Optional[int] = None
-    stint_length: Optional[float] = Field(alias="length")
     end_fuel: Optional[float] = None
+    length: Optional[float] = None
     refuel_amount: Optional[float] = None
     pit_service_duration: Optional[float] = None
     tire_change: Optional[bool] = None
 
+    class Config:
+        from_attributes = True
+
 
 class StintRead(StintBase):
-    pass
+    id: int
+    session_id: int
+    laps: List[Lap] = []
 
 
 class StintCreate(StintBase):
-    pass
+    session_id: int
 
 
-class StintUpdate(StintBase):
-    pass
+class StintUpdate(BaseModel):
+    end_position: Optional[int] = None
+    end_fuel: Optional[float] = None
+    refuel_amount: Optional[float] = None
+    tire_change: Optional[bool] = None
+    repairs: Optional[bool] = None
+    pit_service_duration: Optional[float] = None
+    incidents: Optional[int] = None
+    length: Optional[float] = None
