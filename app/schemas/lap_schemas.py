8d52cc1c5afb534ab092dac1186a714dcf127266
Kamden <kamden@agada-medical.com>
2025-10-18 13:25:50 -0500

    updated LapCreate

--- app/schemas/lap_schemas.py
+++ app/schemas/lap_schemas.py
@@ -2,9 +2,8 @@ from pydantic import BaseModel, Field, ConfigDict
 from typing import Optional
 
 class LapBase(BaseModel):
-    stint_id: int
     lap_number: int = Field(..., alias='number')
     time:float
 
 class LapCreate(LapBase):
-    pass
+    stint_id: int
