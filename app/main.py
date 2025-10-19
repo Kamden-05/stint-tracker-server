fb4fe21713e538a33d59e137268bd260eafe9a32
Kamden <kamden@agada-medical.com>
2025-10-18 20:07:39 -0500

    reordered routes for docs

--- app/main.py
+++ app/main.py
@@ -6,8 +6,8 @@ app = FastAPI()
 
 app.include_router(session_router.router)
 app.include_router(stint_session_router.router)
-app.include_router(lap_router.router)
 app.include_router(stint_router.router)
+app.include_router(lap_router.router)
 
 
 @app.get("/")
