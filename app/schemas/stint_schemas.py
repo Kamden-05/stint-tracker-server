b993b7e762f626005c1dfd3620fe0c3b5f4c756f
Kamden-05 <kamdenw05@gmail.com>
2025-10-27 17:58:41 -0500

    updated StintRead schemas

--- app/schemas/stint_schemas.py
+++ app/schemas/stint_schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, computed_field
 from typing import List, Optional
 from app.schemas.lap_schemas import LapBase as Lap
 
@@ -18,10 +18,29 @@ class StintRead(StintBase):
     session_id: int
     laps: List[Lap] = []
 
-    end_time: float
-    end_position: float
-    end_fuel: float
-    end_incidents: float
+    end_time: Optional[float] = None
+    end_position: Optional[float] = None
+    end_fuel: Optional[float] = None
+    end_incidents: Optional[float] = None
+
+    @computed_field
+    def duration(self) -> Optional[float]:
+        if self.end_time is None:
+            return None
+
+        end = self.end_time
+
+        if end < self.start_time:
+            end += 86400
+
+        return end - self.start_time
+    
+    @computed_field
+    def incidents(self) -> Optional[int]:
+        if self.end_incidents is None:
+            return None
+        else:
+            return self.end_incidents - self.start_incidents
 
     class Config:
         from_attributes = True
