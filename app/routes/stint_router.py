e57a2f49a199874a1b1275c755a70461d8f8779f
Kamden-05 <kamdenw05@gmail.com>
2025-10-27 18:22:10 -0500

    updated response models for stint endpoints

--- app/routes/stint_router.py
+++ app/routes/stint_router.py
@@ -2,7 +2,7 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-from app.schemas.stint_schemas import StintUpdate
+from app.schemas.stint_schemas import StintRead, StintCreate, StintUpdate
 
 from app.database.db import get_db
 from app.models.stint_model import Stint
@@ -13,7 +13,11 @@ router = APIRouter(prefix="/stints", tags=["stints"])
 DbSession = Annotated[Session, Depends(get_db)]
 
 
-@router.get("")
+@router.get(
+    "",
+    response_model=[StintRead],
+    response_model_exclude={"end_time", "end_position", "end_fuel", "end_incidents"},
+)
 def get_stints(db: DbSession):
     stints = stint_crud.get_many(db)
     return stints
@@ -27,6 +31,12 @@ def update_stint(stint_id: int, stint_update: StintUpdate, db: DbSession):
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Stint with id {stint_id} not found",
+        )
+
+    if stint.is_complete:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail=f"Stint with id {stint_id} can no longer be updated",
         )
 
     try:
